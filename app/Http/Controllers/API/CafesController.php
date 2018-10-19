1947376a45aea1acc7be7bacb275e2455c97db03
zhangweipeng <zhangweipengphp@163.com>
2018-10-20 01:56:36 +0800

    update

---
+++ app/Http/Controllers/API/CafesController.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangweipeng
+ * Date: 2018/10/18
+ * Time: 11:43 PM
+ */
+
+namespace app\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Request;
+use App\Models\Cafe;
+
+class CafesController extends Controller
+{
+     public function getCafes(){
+          $cafes = Cafe::all();
+          return response()->json($cafes);
+     }
+
+     public function getCafe($id){
+          $cafe = Cafe::where('id', '=', $id)->first();
+          return response()->json($cafe);
+     }
+
+     public function postNewCafe(){
+          $cafe = new Cafe();
+
+          $cafe->name     = Request::get('name');
+          $cafe->address  = Request::get('address');
+          $cafe->city     = Request::get('city');
+          $cafe->state    = Request::get('state');
+          $cafe->zip      = Request::get('zip');
+
+          $cafe->save();
+
+          return response()->json($cafe, 201);
+     }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,12 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
+     //test
      Route::get('/user', function( Request $request ){
           return $request->user();
      });
+     Route::get('/cafes', 'API\CafesController@getCafes');
+     Route::post('/cafes', 'API\CafesController@postNewCafe');
+     Route::get('/cafes/{id}', 'API\CafesController@getCafe');
+
 });
